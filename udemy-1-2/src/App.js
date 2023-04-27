52b01cbff475815c11c3f560f7adc0300933c000
React <3809164@gmail.com>
2023-04-27 15:09:45 +0300

    form2

--- udemy-1-2/src/App.js
+++ udemy-1-2/src/App.js
@@ -2,10 +2,16 @@ import React from "react";
 import Expenses from "./Components/Expenses/Expenses";
 import NewExpense from "./Components/NewExpense/NewExpense";
 
-const App = () => {
+const App = (props) => {
+
+    const addExpenseHandler = (expense) => {
+        console.log("jgjgk")
+        console.log(expense)
+    }
+
     return (
         <div className={"w-full h-screen bg-black/60 p-4"}>
-            <NewExpense/>
+            <NewExpense onAddExpense={addExpenseHandler}/>
             <Expenses />
         </div>
     )
